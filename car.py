7e1a584e1a216a82ae10fc36eb2dcefd597eb638
chonsukp <chonsukp@gmail.com>
2023-12-05 19:54:57 +1000

    Added car.py

---
+++ car.py
@@ -0,0 +1,69 @@
+class Car:
+    # Constructor
+    def __init__(self, color, make):
+        self._color = color
+        self.make = make
+        print("Hello")
+        print(self)
+
+    def get_color(self):
+        # condition fulfilled
+        return self._color
+    
+    def set_color(self, color):
+        # conditions fulfilled
+        self._color = color
+
+    def run(self):
+        print(f"{self.make} is running! Vroom Vroom!")
+
+
+my_car = Car("black", "tesla")
+
+print(my_car.get_color())
+
+my_car.set_color("red")
+
+print(my_car.get_color())
+
+# print(my_car._color)
+# my_car.color = "blue"
+# print(my_car._color)
+# print(my_car._make)
+
+# (my_car.run())
+
+# your_car = Car("White", "Toyota")
+
+# print(your_car.make)
+
+# your_car.run()
+
+class PetrolCar(Car):
+    def __init__(self, color, make, capacity_of_tank):
+        super().__init__(color, make)
+        self.capacity_of_tank = capacity_of_tank
+
+    def make_turn(self):
+        print("I am making a turn")
+
+
+my_petrol_car = PetrolCar("silver", "BMW", 40)
+
+print(my_petrol_car.capacity_of_tank)
+
+my_petrol_car.run()
+my_petrol_car.make_turn()
+
+class ElectricCar(Car):
+    # Overriding
+    def run(self):
+        print("I run silently. No Vrooming!")
+
+    # def run(self, distance):
+        # print(f"If I ran for {distance} km")
+
+my_electric_car = ElectricCar("yellow", "Teslad")
+
+my_electric_car.run()
+# my_electric_car.run(10)
